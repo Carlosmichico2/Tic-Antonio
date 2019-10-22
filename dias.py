9ffd723c395826cc38998699b86dbb543b27e9b9
Carlosmichico2 <56585069+Carlosmichico2@users.noreply.github.com>
2019-10-22 11:38:55 +0200

    Add files via upload

---
+++ dias.py
@@ -0,0 +1,18 @@
+def dia():
+    print "dime un numero del 1 al 7"
+    a=input ("A= ")
+    if a==1:
+        print "monday"
+    if a==2:
+        print "tuesday"
+    if a==3:
+        print "wensday"
+    if a==4:
+        print "thursday"
+    if a==5:
+        print "friday"
+    if a==6:
+        print "saturday"
+    if a==7:
+        print "sunday"
+dia()
